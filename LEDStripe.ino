57b9ab9559d414e55389c762913102e873ea2a05
Tim <blombler008@gmail.com>
2018-04-05 19:04:20 +0200

    Final commit today

---
+++ LEDStripe.ino
@@ -0,0 +1,22 @@
+/* #import <Arduino.h>
+uint8_t pinRGB[3] = {18,19,20}; */
+
+void setup(void){/* 
+    pinMode(pinRGB[0], OUTPUT);
+    pinMode(pinRGB[1], OUTPUT);
+    pinMode(pinRGB[2], OUTPUT); */
+}
+void loop(){
+    /* analogWrite(pinRGB[0],  255);
+    analogWrite(pinRGB[1],  0);
+    analogWrite(pinRGB[2],  0);
+    delay(500);
+    analogWrite(pinRGB[1],  255);
+    analogWrite(pinRGB[0],  0);
+    analogWrite(pinRGB[2],  0);
+    delay(500);
+    analogWrite(pinRGB[2],  255);
+    analogWrite(pinRGB[1],  0);
+    analogWrite(pinRGB[0],  0);
+    delay(500); */
+}
